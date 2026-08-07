8c0423e555af89010bde177df67aea1084e46eeb
taishi-yamasaki <87109349+t-yamsaki@users.noreply.github.com>
2026-08-08 07:22:01 +0900

    feat: export manifold ranking loss

--- src/neembed/__init__.py
+++ src/neembed/__init__.py
@@ -1,5 +1,9 @@
 """neembed package."""
 
+from neembed.losses import ManifoldMultipleNegativesRankingLoss
 from neembed.model import ManifoldSentenceTransformer
 
-__all__ = ["ManifoldSentenceTransformer"]
+__all__ = [
+    "ManifoldMultipleNegativesRankingLoss",
+    "ManifoldSentenceTransformer",
+]
