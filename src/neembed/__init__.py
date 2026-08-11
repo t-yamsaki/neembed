d71a994391cb58c29434edf4e8fc526aeff501f6
taishi-yamasaki <87109349+t-yamsaki@users.noreply.github.com>
2026-08-11 09:31:20 +0900

    Export manifold embedding evaluator

--- src/neembed/__init__.py
+++ src/neembed/__init__.py
@@ -1,10 +1,12 @@
 """neembed package."""
 
+from neembed.evaluator import ManifoldEmbeddingEvaluator
 from neembed.losses import ManifoldMultipleNegativesRankingLoss
 from neembed.model import ManifoldSentenceTransformer
 from neembed.trainer import ManifoldTrainer
 
 __all__ = [
+    "ManifoldEmbeddingEvaluator",
     "ManifoldMultipleNegativesRankingLoss",
     "ManifoldSentenceTransformer",
     "ManifoldTrainer",
